848dbcab6e2a425807ac7d333f24121094b2e173
patelharsh2953-cmyk <patelharsh2953@gmail.com>
2026-08-29 21:59:56 +0530

    feat: implement database connection utility using mongoose

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    // await mongoose.connect(process.env.MONOG_URL).then(() => console.log("DB Connected"));
-    await mongoose.connect("mongodb+srv://patelharsh2953_db_user:9h5H2Rsbpoul5xPN@cluster0.mektegj.mongodb.net/food_del?appName=Cluster0").then(() => console.log("DB Connected"));
+    await mongoose.connect(process.env.MONOG_URL).then(() => console.log("DB Connected"));
 }
 
 
